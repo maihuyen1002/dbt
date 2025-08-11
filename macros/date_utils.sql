c608c6a6bc07830d279c5c1cb9534f130742546a
maikhanhhuyen1002@gmail.com <maikhanhhuyen1002@gmail.com>
2025-08-11 22:25:41 +0000

    ---fix

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -1,5 +1,6 @@
-(% macro function1(x) %)
-     CASE WHEN TO_TIMESTAMP({{x}}) < CURRENT_DATE THEN 'PAST'
-     ELSE 'FUTURE' END
-(%endmacro%)
-
+{% macro function1(x) %}
+    CASE 
+        WHEN TO_TIMESTAMP({{ x }}) < CURRENT_DATE THEN 'PAST'
+        ELSE 'FUTURE' 
+    END
+{% endmacro %}
