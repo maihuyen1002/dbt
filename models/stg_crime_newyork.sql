ba6e457d78efeec2b178a48782976c981eb430b5
huyen mai <maikhanhhuyen1002@gmail.com>
2026-07-30 08:55:53 +0000

    -- update models and source

--- models/stg_crime_newyork.sql
+++ models/stg_crime_newyork.sql
@@ -1,6 +1,8 @@
 with cte as (
     select *
     from {{ source('USCRIME', 'URBAN_CRIME_INCIDENT_LOG') }}
+    where city = 'New York'
+         and OFFENSE_CATEGORY IN  ('Theft'  , 'Driving Under The Influence')
 )
 
 SELECT *
